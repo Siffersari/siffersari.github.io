90f2ce3e10063b1be5e9d6b03d5b051e1ace641f
Leewel Mwangi <leewelkarani@gmail.com>
2026-08-06 10:39:06 +0300

    Publish recruiter portfolio

---
+++ _next/static/chunks/app/work/page-c1f8c1ee283af9b1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[534],{4747:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,3509)),Promise.resolve().then(s.bind(s,9818))}},function(e){e.O(0,[972,215,565,971,117,744],function(){return e(e.s=4747)}),_N_E=e.O()}]);
